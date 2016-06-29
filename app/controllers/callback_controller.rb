03f5ef44141aca19b7e037657e2fe23a02fc9227
Pawel Niewiadomski <11110000b@gmail.com>
2016-06-29 21:38:49 +0200

    callback controller calls promoter api

--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -1,5 +1,12 @@
 class CallbackController < ApplicationController
   def handle
+    # File.open(Rails.root.join('payload.json'), 'w') do |file|
+    #   file.write(params[:issue].to_json)
+    # end
 
+    promoter = Promoter::Client.new(Rails.application.secrets.promoter_key)
+    promoter.contact.create(email: params[:issue][:fields][:reporter][:emailAddress], # required
+                            first_name: params[:issue][:fields][:reporter][:displayName],
+                            send: true)
   end
 end
