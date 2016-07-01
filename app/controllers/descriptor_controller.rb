5a04355e9a8cbeaff836bd5ce0dfa126c2dbbc74
Pawel Niewiadomski <11110000b@gmail.com>
2016-07-01 11:45:58 +0200

    fixed descriptor resetting to example.org for base url

--- app/controllers/descriptor_controller.rb
+++ app/controllers/descriptor_controller.rb
@@ -2,7 +2,7 @@ class DescriptorController < ApplicationController
 
 	def show
 		render :show, locals: {
-			base_url: 'https://' + ApplicationController.renderer.defaults[:http_host],
+			base_url: 'https://' + Rails.application.secrets.http_host,
 			plugin_key: PluginKeyService::PLUGIN_KEY,
       client_id: Rails.application.secrets.client_id,
 			plugin_name: 'Surveys for JIRA Service Desk' + (Rails.env.production? ? '' : ' [Development]')
