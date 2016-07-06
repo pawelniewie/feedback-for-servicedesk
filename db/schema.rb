f3baecb187f81adcd352977a6f661bfc71271f51
Pawel Niewiadomski <11110000b@gmail.com>
2016-07-06 22:00:07 +0200

    added JwtUserInfo model

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160606074334) do
+ActiveRecord::Schema.define(version: 20160706124334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +21,14 @@ ActiveRecord::Schema.define(version: 20160606074334) do
     t.string "shared_secret"
     t.string "product_type"
     t.string "base_url"
+    t.string "api_base_url"
     t.index ["client_key"], name: "index_jwt_tokens_on_client_key", using: :btree
+  end
+
+  create_table "jwt_user_infos", force: :cascade do |t|
+    t.integer "jwt_user_id", null: false
+    t.string  "email",       null: false
+    t.string  "time_zone",   null: false
   end
 
   create_table "jwt_user_settings", force: :cascade do |t|
@@ -37,5 +43,22 @@ ActiveRecord::Schema.define(version: 20160606074334) do
     t.string  "display_name"
   end
 
+  create_table "project_configurations", force: :cascade do |t|
+    t.integer  "jwt_token_id"
+    t.integer  "project_id",                  null: false
+    t.string   "product_name", default: "",   null: false
+    t.string   "language",     default: "en", null: false
+    t.string   "reply_to",                    null: false
+    t.string   "from",                        null: false
+    t.string   "subject",                     null: false
+    t.text     "introduction", default: "",   null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.index ["jwt_token_id", "project_id"], name: "index_project_configurations_on_jwt_token_id_and_project_id", unique: true, using: :btree
+    t.index ["jwt_token_id"], name: "index_project_configurations_on_jwt_token_id", using: :btree
+  end
+
+  add_foreign_key "jwt_user_infos", "jwt_users", on_delete: :cascade
   add_foreign_key "jwt_user_settings", "jwt_users", on_delete: :cascade
+  add_foreign_key "project_configurations", "jwt_tokens"
 end
