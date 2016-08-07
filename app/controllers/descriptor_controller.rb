dfa43b9f388021ca1ea6b09646927cfd0c285172
Pawel Niewiadomski <11110000b@gmail.com>
2016-08-07 20:14:36 +0200

    changed plugin name to be shorter (it will appear as username)

--- app/controllers/descriptor_controller.rb
+++ app/controllers/descriptor_controller.rb
@@ -5,7 +5,7 @@ class DescriptorController < ApplicationController
 			base_url: 'https://' + Rails.application.secrets.http_host,
 			plugin_key: PluginKeyService::PLUGIN_KEY,
       client_id: Rails.application.secrets.client_id,
-			plugin_name: 'Customer Feedback for JIRA Service Desk' + (Rails.env.production? ? '' : ' [Development]')
+			plugin_name: 'Customer Feedback' + (Rails.env.production? ? '' : ' [Development]')
 		}
 	end
 
