11f4f30574f26e688466b2a340d5cbb425f819b9
Pawel Niewiadomski <11110000b@gmail.com>
2016-08-07 20:39:51 +0200

    Revert "migrating to new plugin key"
    This reverts commit 59670fcc41679b7f8ba2ae45134ad4637ee25b10.

--- app/services/plugin_key_service.rb
+++ app/services/plugin_key_service.rb
@@ -1,5 +1,5 @@
 module PluginKeyService
 
-	PLUGIN_KEY = 'customer-feedback-for-jira-servicedesk-cloud' + (Rails.env.production? ? '' :  '.' + Rails.env)
+	PLUGIN_KEY = 'customer-feedback-for-servicedesk-cloud' + (Rails.env.production? ? '' :  '.' + Rails.env)
 
 end
